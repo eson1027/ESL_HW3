d57d3ff6ef9df1ffe478b265a5c4c749c66b168b
eson1027 <56966423+eson1027@users.noreply.github.com>
2024-05-12 19:18:32 +0800

    upload hw3_1

---
+++ gaussian_hw3_1/SobelFilter.h
@@ -0,0 +1,33 @@
+#ifndef SOBEL_FILTER_H_
+#define SOBEL_FILTER_H_
+#include <systemc>
+using namespace sc_core;
+
+#ifndef NATIVE_SYSTEMC
+#include <cynw_p2p.h>
+#endif
+
+#include "filter_def.h"
+
+class SobelFilter: public sc_module
+{
+public:
+	sc_in_clk i_clk;
+	sc_in < bool >  i_rst;
+#ifndef NATIVE_SYSTEMC
+	cynw_p2p< rgb_t >::in i_rgb;
+	cynw_p2p< result_t >::out o_result;
+#else
+	sc_fifo_in< rgb_t > i_rgb;
+	sc_fifo_out< result_t > o_result;
+#endif
+
+	SC_HAS_PROCESS( SobelFilter );
+	SobelFilter( sc_module_name n );
+	~SobelFilter();
+private:
+	void do_filter();
+  //sc_dt::sc_uint<17> val;
+  sc_dt::sc_uint<8> buffer_gray[BUFFER_SIZE];
+};
+#endif
---
+++ gaussian_hw3_1/filter_def.h
@@ -0,0 +1,11 @@
+#ifndef FILTER_DEF_H_
+#define FILTER_DEF_H_
+
+#define MASK_N 1
+#define MASK_X 5
+#define MASK_Y 5
+#define BUFFER_SIZE 25
+typedef sc_uint<24> rgb_t;
+typedef sc_uint<32> result_t;
+
+#endif
